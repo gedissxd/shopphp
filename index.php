3471c75e65084b84e3e398b503131fc46c593444
Gediminas <gediss.lolas78@gmail.com>
2025-02-19 20:37:12 +0200

    if no products just print nothing

--- index.php
+++ index.php
@@ -6,6 +6,8 @@ $sql = "SELECT * FROM `products_list`";
 $result = mysqli_query($conn, $sql);
 if (mysqli_num_rows($result) > 0) {
     $productList = mysqli_fetch_all($result, MYSQLI_ASSOC);
+} else {
+    $productList = [];
 }
 
 require "views/index.view.php";
