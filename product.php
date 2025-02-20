cccc73f480cdbf1e7da894a9d02de5cf67a99272
Gediminas <gediss.lolas78@gmail.com>
2025-02-20 19:34:13 +0200

    product page is dynamic

--- product.php
+++ product.php
@@ -1,3 +1,13 @@
 <?php
+require "database/database.php";
 
+$productId = $_GET['id'];
+
+$sql = "SELECT * FROM `products_list` WHERE `id` = $productId";
+$result = mysqli_query($conn, $sql);
+if (mysqli_num_rows($result) > 0) {
+    $product = mysqli_fetch_assoc($result);
+} else {
+    $product = [];
+}
 require "views/product.view.php";
--- views/cart.view.php
+++ views/cart.view.php
@@ -1,87 +1,44 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
 </head>
+
 <body class="bg-white overflow-y-scroll">
-<?php require 'components/navbar.php'; ?>
-<div class="flex justify-center items-center">
+    <?php require 'components/navbar.php'; ?>
+    <div class="flex justify-center items-center">
 
 
-    <div class="bg-red-300 w-2xl mt-5 p-5">
-    <h1 class="flex justify-center items-center text-white text-2xl font-bold ">
-        CART ITEMS
-    </h1>
-    <div class="flex justify-between mt-2">
-        <div class="w-1/8  flex">
-        <img src="https://tailwindui.com/plus-assets/img/ecommerce-images/category-page-04-image-card-01.jpg" alt="">
-        <labe class="mt-7 ml-5">Product name</label>
-        </div>
-        
-        
-        <label class="mt-7">Count: 2</label>
-        <label class="mt-7">Price: 50$</label>
-        <div class="mt-7">
-        <button class="bg-red-900 text-white px-2 rounded-full">-</button> 
-        <button class="bg-red-900 text-white px-2 rounded-full">+</button> 
-        <button class="bg-red-900 text-white px-2">Remove</button> 
-        </div>
-      
-    </div>
-    <div class="flex justify-between mt-2">
-        <div class="w-1/8  flex">
-        <img src="https://tailwindui.com/plus-assets/img/ecommerce-images/category-page-04-image-card-01.jpg" alt="">
-        <labe class="mt-7 ml-5">Product name</label>
-        </div>
-        
-        
-        <label class="mt-7">Count: 2</label>
-        <label class="mt-7">Price: 50$</label>
-        <div class="mt-7">
-        <button class="bg-red-900 text-white px-2 rounded-full">-</button> 
-        <button class="bg-red-900 text-white px-2 rounded-full">+</button> 
-        <button class="bg-red-900 text-white px-2">Remove</button> 
-        </div>
-      
-    </div>
-    <div class="flex justify-between mt-2">
-        <div class="w-1/8  flex">
-        <img src="https://tailwindui.com/plus-assets/img/ecommerce-images/category-page-04-image-card-01.jpg" alt="">
-        <labe class="mt-7 ml-5">Product name</label>
-        </div>
-        
-        
-        <label class="mt-7">Count: 2</label>
-        <label class="mt-7">Price: 50$</label>
-        <div class="mt-7">
-        <button class="bg-red-900 text-white px-2 rounded-full">-</button> 
-        <button class="bg-red-900 text-white px-2 rounded-full">+</button> 
-        <button class="bg-red-900 text-white px-2">Remove</button> 
-        </div>
-      
-    </div>
-    <div class="flex justify-between mt-2">
-        <div class="w-1/8  flex">
-        <img src="https://tailwindui.com/plus-assets/img/ecommerce-images/category-page-04-image-card-01.jpg" alt="">
-        <labe class="mt-7 ml-5">Product name</label>
-        </div>
-        
-        
-        <label class="mt-7">Count: 2</label>
-        <label class="mt-7">Price: 50$</label>
-        <div class="mt-7">
-        <button class="bg-red-900 text-white px-2 rounded-full">-</button> 
-        <button class="bg-red-900 text-white px-2 rounded-full">+</button> 
-        <button class="bg-red-900 text-white px-2">Remove</button> 
-        </div>
-      
-    </div>
+        <div class="bg-red-300 w-2xl mt-5 p-5">
+            <h1 class="flex justify-center items-center text-white text-2xl font-bold ">
+                CART ITEMS
+            </h1>
+            <div class="flex justify-between mt-2">
+                <div class="w-1/8  flex">
+                    <img src="https://tailwindui.com/plus-assets/img/ecommerce-images/category-page-04-image-card-01.jpg"
+                        alt="">
+                    <labe class="mt-7 ml-5">Product name</label>
+                </div>
 
-    <div>
-        <button class="bg-sky-500 text-white w-full mt-16 p-4 rounded-full">Checkout</button>
-    </div>
+
+                <label class="mt-7">Count: 2</label>
+                <label class="mt-7">Price: 50$</label>
+                <div class="mt-7">
+                    <button class="bg-red-900 text-white px-2 rounded-full">-</button>
+                    <button class="bg-red-900 text-white px-2 rounded-full">+</button>
+                    <button class="bg-red-900 text-white px-2">Remove</button>
+                </div>
+
+            </div>
+
+
+            <div>
+                <button class="bg-sky-500 text-white w-full mt-16 p-4 rounded-full">Checkout</button>
+            </div>
 </body>
+
 </html>
